1c3b0bfd59f52e47deca8b30c05096f2edce4c80
AnasHafsi <AnasHafsii@gmail.com>
2019-06-27 22:04:49 +0100

    Arduino optimisation

--- walk.ino
+++ walk.ino
@@ -1,12 +1,14 @@
-#define bit0
-#define bit1
-#define bit2
-#define IN1
-#define IN2
-#define IN3
-#define IN4
-#define SP1
-#define SP2
+#define bit0 4
+#define bit1 3
+#define bit2 2
+#define IN1 9
+#define IN2 10
+#define IN3 11
+#define IN4 12
+#define SP1 6
+#define SP2 5
+int dip = 0;
+int dim = 0;
 
 void setup()
 {
@@ -19,20 +21,33 @@ void setup()
     pinMode(IN4, OUTPUT);
     pinMode(SP1, OUTPUT);
     pinMode(SP2, OUTPUT);
+    Serial.begin(9600);
 }
 void STOP()
 {
+    Serial.println("STOP");
     analogWrite(SP1, 0);
     analogWrite(SP2, 0);
 }
 void LEFT()
 {
+    Serial.println("LEFT");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN3, HIGH);
+    analogWrite(SP1, 0);
+    analogWrite(SP2, 255);
 }
 void RIGHT()
 {
+    Serial.println("RIGHT");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN3, HIGH);
+    analogWrite(SP1, 255);
+    analogWrite(SP2, 0);
 }
 void UP()
 {
+    Serial.println("UP");
     digitalWrite(IN1, HIGH);
     digitalWrite(IN3, HIGH);
     analogWrite(SP1, 255);
@@ -40,16 +55,40 @@ void UP()
 }
 void BACK()
 {
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN3, HIGH);
+    dim=0;
+    Serial.println("BACK");
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN4, HIGH);
     analogWrite(SP1, 255);
     analogWrite(SP2, 255);
 }
 void diagM()
 {
+    Serial.println("diagM");
+    
+    if (dim==0)
+    {
+        Serial.println("dim is 0");
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN3, HIGH);
+        analogWrite(SP1, 255);
+        analogWrite(SP2, 50);
+        delay(100);
+    }
+    dim++;
+    analogWrite(SP1, 255);
+    analogWrite(SP2, 255);
 }
 void diagP()
 {
+    Serial.println("diagP");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN3, HIGH);
+    analogWrite(SP1, 50);
+    analogWrite(SP2, 250);
+    delay(100);
+    analogWrite(SP1, 255);
+    analogWrite(SP2, 255);
 }
 void loop()
 {
@@ -67,8 +106,10 @@ void loop()
         }
         else
         {
-            if (C == HIGH)
-                diagP() else UP();
+            if (c == HIGH)
+                diagP();
+            else
+                UP();
         }
     }
     else
